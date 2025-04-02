baf30455c440404dfe3092f805c96851c0869884
Oswaldo Galicia <o.galicia.0812@gmail.com>
2025-04-01 18:35:15 -0600

    fixing broken button error

--- admin/index.php
+++ admin/index.php
@@ -6,7 +6,7 @@
     <main class="contenedor seccion">
         <h1>Administrador de bienes raices</h1>
 
-        <a href="./propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+        <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
     </main>
     
     <?php incluirTemplate('footer'); ?>
